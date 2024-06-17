effde375d4a2127ed9ae8381b6c693baa5b0660a
Wellington Henrique <wellingtonhlc@gmail.com>
2024-06-17 20:40:58 -0300

    Estrutura Visual: Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,17 @@ import { globalStyles } from "../styles/global";
 
 globalStyles();
 
+import logoImage from "../assets/logo.svg";
+import { Container, Header } from "../styles/pages/app";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img src={logoImage.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
