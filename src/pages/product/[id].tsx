e0445bd0eb54c8452d42f9fdffa67351eb4d0390
Wellington Henrique <wellingtonhlc@gmail.com>
2024-06-25 22:12:40 -0300

    Produto & Checkout: Estrutura da página de produto:

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,30 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
-    const { query } = useRouter();
+  const { query } = useRouter();
 
-    return (
-        <h1>Product { JSON.stringify(query) }</h1>
-    )
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          dolorem hic veniam impedit nesciunt voluptatibus asperiores quibusdam
+          explicabo, corporis debitis esse saepe dolor itaque deleniti eum quia
+          enim magni voluptate.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
