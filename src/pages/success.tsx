4a2ef52ae3699531c673fd59372c97d3ce2b442d
Wellington Henrique <wellingtonhlc@gmail.com>
2024-07-16 20:53:58 -0300

    Produto & Checkout: Estrutura de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
-    return (
-        <h1>Success</h1>
-    )
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhull <strong>nome do usuário</strong>, sua{" "}
+        <strong>descrição do produto</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  );
 }
